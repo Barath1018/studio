b35959de9686fcdf6e42defcf00ccbef58ccbd4d
Barath Senthil <barath.senthil1602@gmail.com>
2025-08-27 17:43:56 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/layout.tsx
+++
@@ -1,6 +0,0 @@
-import DashboardPage from '@/components/dashboard/dashboard-page';
-import { ReactNode } from 'react';
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <DashboardPage>{children}</DashboardPage>;
-}
